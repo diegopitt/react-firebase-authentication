3de68cf80eeedf4787a897e1161be5213f7e867e
Diego <diego.pittaluga@gmail.com>
2019-06-10 12:58:29 -0700

    updates

--- src/MainConnector.js
+++ src/MainConnector.js
@@ -6,36 +6,33 @@ import { signin } from "./pages/signin";
 import { contracts } from "./pages/contracts";
 import { messages } from "./pages/messages";
 import { authWrapper } from "./auth/AuthWrapper";
+
 export const MainConnector = authWrapper(class extends Component {
 
   selectComponent = (routeProps) => {
-    const wrap = (Component) =>
-      <Component {...this.props}  {...routeProps}>
-      </Component>
-      switch (routeProps.match.params.section) {
-        case "jobs":
-          return wrap(jobs);
-        case "proposals":
-          return wrap(proposals);
-        case "contracts":
-          return wrap(contracts);
-        case "messages":
-          return wrap(messages);
-        case "signin":
-          return wrap(signin);
-        default:
-          return <Redirect to="/notfound" />
-      }
+    const wrap = (Component) => <Component {...this.props}  {...routeProps}></Component>;
+    if (!this.props.isAuthenticated) return wrap(signin);
+    switch (routeProps.match.params.section) {
+      case "jobs":
+        return wrap(jobs);
+      case "proposals":
+        return wrap(proposals);
+      case "contracts":
+        return wrap(contracts);
+      case "messages":
+        return wrap(messages);
+      case "signin":
+        return wrap(signin);
+      default:
+        return <Redirect to="/notfound" />
+    }
   }
 
   render() {
     return <Switch>
       <Route path={"/dashboard/:section?"}
         render={routeProps =>
-          if (routeProps) {console.log('here')}
-          this.selectComponent(routeProps)
-        } />
+          this.selectComponent(routeProps)} />
     </Switch>
   }
-
 })
